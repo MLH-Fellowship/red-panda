ea8b84b71f95685dcef63298eaaa34f9b56fa604
David Knox <dakdave26@gmail.com>
2020-10-26 15:26:40 -0400

    Add player movement and first level draft
    This commit adds a player with a movement script, and a draft level for testing purposes. There are also basic materials in place for the testing environment.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float walkSpeed = 8f;
+    public float jumpSpeed = 5f;
+    bool isGrounded;
+
+    Rigidbody rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        GroundChecker();
+        WalkHandler();
+        JumpHandler();
+    }
+
+    void WalkHandler() 
+    {
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(hAxis * walkSpeed, rb.velocity.y, vAxis * walkSpeed);
+        rb.velocity = movement;
+    }
+
+    void JumpHandler() 
+    {
+        float jAxis = Input.GetAxis("Jump");
+
+        if (jAxis > 0f && this.isGrounded)
+        {
+            Vector3 jumpVector = new Vector3(0f, jumpSpeed, 0f);
+            rb.AddForce(jumpVector, ForceMode.Impulse);
+        }
+    }
+
+    void GroundChecker()
+    {
+        RaycastHit hit;
+        float rayDist = 0.65f;
+        Vector3 rayDir = new Vector3(0, -1);
+
+        if (Physics.Raycast(transform.position, rayDir, out hit, rayDist))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+}
