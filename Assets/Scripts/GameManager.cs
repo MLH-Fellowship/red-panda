1877cd2c1f4bd3a49fe59cce546a73148d616e61
David Knox <dakdave26@gmail.com>
2020-10-26 16:53:09 -0400

    Adds goal functionality, prefabs, and levels
    This commit adds the function for the goal to change to the next level, with the final level logging a console message to be replaced with game ending functionality later. In addition reusable elements were converted to prefabs and a tree object was created.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        instance = this;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void LevelEnd()
+    {
+        if(SceneManager.sceneCountInBuildSettings == SceneManager.GetActiveScene().buildIndex + 1)
+        {
+            WinGame();
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+    public void WinGame()
+    {
+        Debug.Log("A winner is you!");
     }
 }
