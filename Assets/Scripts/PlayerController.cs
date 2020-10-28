b591533fa52ffe28efb21f7c8ef96b952261b705
Ernest <ernestcas@berkeley.edu>
2020-10-28 10:11:39 -0700

    Layer changes bug fix
    Fixed the layer code in player controller for raycasts

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -76,8 +76,9 @@ public class PlayerController : MonoBehaviour
         RaycastHit hit;
         float rayDist = 0.65f;
         Vector3 rayDir = new Vector3(0, -1);
+        int layerMask = 1 << 0;
 
-        if (Physics.Raycast(transform.position, rayDir, out hit, rayDist) && hit.collider.tag != "Safety" && hit.collider.tag != "FearEntity")
+        if (Physics.Raycast(transform.position, rayDir, out hit, rayDist, layerMask))
         {
             isGrounded = true;
         }
